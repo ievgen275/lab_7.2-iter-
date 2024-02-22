d5f31cfe53ed924579ba42ea6006dedb78050fdb
ievgen275 <ievgen3435@gmail.com>
2024-02-22 19:30:08 +0200

    first commit

---
+++ PR_7.2(iter)/PR_7.2(iter).cpp
@@ -0,0 +1,81 @@
+﻿// Lab_7_2_1.cpp
+// < Катрич Євген >
+// Лабораторна робота № 7.2.1)
+// Опрацювання багатовимірних масивів ітераційними способами.
+// Варіант 6
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+void Create(int** a, const int n, const int k, const int Low, const int High);
+void Print(int** a, const int n, const int k);
+void SearchMinEven(int** a, const int n, const int k, int& minEven);
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int Low = 7;
+	int High = 65;
+	int n, k; // n - row k - col
+
+	cout << "n = "; cin >> n;
+	cout << "k = "; cin >> k;
+
+	int** a = new int* [n];
+	for (int i = 0; i < n; i++)
+		a[i] = new int[k];
+
+	Create(a, n, k, Low, High);
+	Print(a, n, k);
+
+	int minEven;
+	SearchMinEven(a, n, k, minEven);
+
+	cout << "min value = " << minEven << endl;
+
+	for (int i = 0; i < n; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+
+}
+
+void Create(int** a, const int n, const int k, const int Low, const int High)
+{
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < k; j++)
+			a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** a, const int n, const int k)
+{
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < k; j++)
+			cout << setw(4) << a[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void SearchMinEven(int** a, const int n, const int k, int& minEven)
+{
+	minEven = INT_MAX;
+
+	for (int i = 0; i < n; i++) {
+		int maxElemRow = a[i][0];
+		for (int j = 0; j < k; j++)
+		{
+			if (a[i][j] > maxElemRow) {
+				maxElemRow = a[i][j];
+			}
+		}
+		if (maxElemRow < minEven) {
+			minEven = maxElemRow;
+		}
+	}
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_7.2(iter)/PR_7.2(iter).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestSearchMinEven)
+		{
+			const int n = 3;
+			const int k = 4;
+			int Low = 7;
+			int High = 65;
+
+			int** testArray = new int* [n];
+			for (int i = 0; i < n; i++) {
+				testArray[i] = new int[k];
+			}
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < k; j++) {
+					testArray[i][j] = Low + rand() % (High - Low + 1);
+				}
+			}
+
+			int minEven;
+			SearchMinEven(testArray, n, k, minEven);
+
+			Assert::AreEqual(48, minEven);
+
+			for (int i = 0; i < n; i++) {
+				delete[] testArray[i];
+			}
+			delete[] testArray;
+		}
+	};
+}
